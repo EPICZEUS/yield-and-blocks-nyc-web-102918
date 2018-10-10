ee1700fe61e75f5b0ab4298d567ed33d827e325f
Sam B <s77broz@aol.com>
2018-10-10 18:54:02 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(arr)
   i = 0
-  while i < arr.length
-    yield arr[i] if block_given?
-    i += 1
+  if block_given?
+    while i < arr.length
+      yield arr[i]
+      i += 1
+    end
+    arr
+  else
+    puts "No block"
   end
-  arr
 end
 
 # call your method here!
